a3207b94abb0f673f8b5ac29cc3b342b49315beb
Faid Terence JABO <faidterence@outlook.com>
2023-09-14 19:12:04 +0200

    Database Configurations

--- src/config/db.php
+++ src/config/db.php
@@ -6,7 +6,7 @@ require 'dbconfigs.php';
 $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if($con->connect_error) {
-    die("Connection failed: " . $conn -> connect_error);  // die() is a function that stops the script and displays an error
+    die("Connection failed: " . $conn -> connect_error);
 }
 else{
     echo "Connected";
